a85871013c4ffea92453276f9db1eab5448801f9
Praveen Raj <rajp03262@gmail.com>
2023-12-29 12:33:35 +0530

    exercise_tracker_web_application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Navbar />
+          <br />
+          <Routes>
+            <Route path="/" element={<ExercisesList />} />
+            <Route path="/edit/:id" element={<EditExercise />} />
+            <Route path="/create" element={<CreateExercise />} />
+            <Route path="/user" element={<CreateUser />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/edit-exercises.component.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useParams } from 'react-router-dom';
+
+const EditExercise = () => {
+  const { id } = useParams();
+
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/exercises/${id}`)
+      .then((response) => {
+        setUsername(response.data.username);
+        setDescription(response.data.description);
+        setDuration(response.data.duration);
+        setDate(new Date(response.data.date));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get("http://localhost:5000/users/")
+      .then((response) => {
+        if (response.data.length > 0) {
+          setUsers(response.data.map((user) => user.username));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value);
+  };
+
+  const onChangeDate = (date) => {
+    setDate(date);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username: username,
+      description: description,
+      duration: duration,
+      date: date,
+    };
+
+    console.log(exercise);
+
+    axios
+      .post(`http://localhost:5000/exercises/update/${id}`, exercise)
+      .then((res) => console.log(res.data));
+
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <h3>Edit Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={username}
+            onChange={onChangeUsername}
+          >
+            {users.map(function (user) {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={onChangeDescription}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={onChangeDuration}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker selected={date} onChange={onChangeDate} />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Edit Exercise Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditExercise;
